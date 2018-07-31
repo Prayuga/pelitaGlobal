a66f655b0f9e304e01247821a8fee0a97b8d4707
prayuga <prayugao@gmail.com>
2018-07-31 16:40:56 +0700

    Merge origin/master
    Conflicts:
    	web/Statistik/Team/Template Excel Team PCF.xlsx

--- application/controllers/pendataanSiswa.php
+++ application/controllers/pendataanSiswa.php
@@ -28,4 +28,20 @@ class pendataanSiswa extends CI_Controller {
 		$this->load->view('pendataanSiswa/'.$var);
 		$this->load->view('templates/footer');
 	}
+	
+	public function kelas( $var = 'pendataanKelas'){
+
+		//cek halamannya ada apa engga
+		if(!file_exists(APPPATH."views/pendataanSiswa/".$var.'.php')){
+			show_404();
+		}
+
+		//ini array
+		$data['judul'] = $var;
+
+		$this->Login_model->keamanan();
+		$this->load->view('templates/header');
+		$this->load->view('pendataanSiswa/'.$var);
+		$this->load->view('templates/footer');
+	}
 }
---
+++ application/views/pendataanSiswa/pendataanKelas.php
@@ -0,0 +1,15 @@
+<div id="page-wrapper">
+
+    
+       <table id="mytable" class="table table-striped table-bordered" cellspacing="0" width="100%">
+        	<thead>
+        		<tr>
+        			<th>NISN</th>
+                                <th>Nama Siswa</th>
+        		</tr>
+        	</thead>
+			<tbody>
+			</tbody>        	
+        </table>
+</div>
+<!-- /#page-wrapper -->
