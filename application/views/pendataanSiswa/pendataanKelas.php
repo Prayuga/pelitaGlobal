7865f66186f675144999b7dc5759875682b47049
prayuga <prayugao@gmail.com>
2018-08-01 14:16:20 +0700

    Merge origin/master
    Conflicts:
    	web/Statistik/Team/Template Excel Team PCF.xlsx

--- application/views/pendataanSiswa/pendataanKelas.php
+++ application/views/pendataanSiswa/pendataanKelas.php
@@ -1,15 +1,26 @@
 <div id="page-wrapper">
-
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Pendataan Kelas</h1>
+        </div>
+    </div>
+    
+    <div id="panel panel-default">
+        <div class="panel-body">
     
        <table id="mytable" class="table table-striped table-bordered" cellspacing="0" width="100%">
         	<thead>
         		<tr>
-        			<th>NISN</th>
+        			<th>NIS</th>
                                 <th>Nama Siswa</th>
+                                <th>Action</th>
         		</tr>
         	</thead>
 			<tbody>
 			</tbody>        	
         </table>
+        
+        </div>
+    </div>
 </div>
 <!-- /#page-wrapper -->
